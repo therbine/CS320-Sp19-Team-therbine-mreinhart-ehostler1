66198f1767b5453340457ae236c9ba6820f3d81e
Ethan Hostler <ehostler1@ycp.edu>
2019-03-24 12:17:07 -0400

    Coded model of sign in page

--- src/model/SignInPageModel.java
+++ src/model/SignInPageModel.java
@@ -1,5 +1,36 @@
 package model;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+
 public class SignInPageModel {
+	private static Map<String, String> users = new TreeMap<String, String>();
+	
+	public SignInPageModel() {
+		users.put("admin", "admin");
+	}
+	
+	public void addUser(String username, String password) {
+		SignInPageModel.users.put(username, password);
+	}
+	
+	public boolean checkForUser(String username) {
+		return SignInPageModel.users.containsKey(username);
+	}
+	
+	public String getPassword(String username) {
+		return SignInPageModel.users.get(username);
+	}
+	
+	public void deleteUser(String username) {
+		SignInPageModel.users.remove(username);
+	}
+	
+	public Set<String> getSetOfUsers() {
+		return SignInPageModel.users.keySet();
+	}
+	
 
 }
