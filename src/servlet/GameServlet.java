e3b973ff65c54097110f2346972ac393d4d7fd90
Thomas Herbine <therbine@ycp.edu>
2019-03-24 16:43:08 -0400

    Added code to game servlet

--- src/servlet/GameServlet.java
+++ src/servlet/GameServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import controller.GameController;
 import model.GameModel;
 
-public class GameServlet {
+public class GameServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
