b6ef487430a2a20d15b1fcf596d726ea17edcc73
Thomas Herbine <therbine@ycp.edu>
2019-03-24 19:11:07 -0400

    Fixed encapsulation in game controller

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -3,14 +3,14 @@ package controller;
 import model.GameModel;
 
 public class GameController {
-	public GameModel model;
+	private GameModel model;
 	
 	public void setModel(GameModel model) {
 		this.model = model;
 	}
 	
 	public String getMessage() {
-		String result = "";
+		String result = "Test";
 		return result;
 	}
 }
