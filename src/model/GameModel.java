d0d8476c82f5caa1dcedb3af4744302dfef0fa48
Michael Reinhart <mreinhart@ycp.edu>
2019-03-24 18:50:27 -0400

    added intro to model

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -1,5 +1,9 @@
 package model;
 
 public class GameModel {
-
+	public String getIntro() {
+		return "You wake up on a beach with nothing but a rusty sword and the clothes on your back. "
+				+ "To the north there seams to be a dense jungle while to your east and west there’s "
+				+ "a bunch of washed up wreckage. ";
+	}
 }
