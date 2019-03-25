a1fef12adcf30d3b5e1733826ff756336f941a8b
Ethan Hostler <ehostler1@ycp.edu>
2019-03-25 10:57:53 -0400

    fix for sign in page controller junit

--- junit/controllerTests/SignInPageControllerTest.java
+++ junit/controllerTests/SignInPageControllerTest.java
@@ -9,30 +9,28 @@ import controller.SignInPageController;
 import model.SignInPageModel;
 
 public class SignInPageControllerTest {
-	public class NumbersControllerTest {
-		private SignInPageModel model;
-		private SignInPageController controller;
+	private SignInPageModel model;
+	private SignInPageController controller;
 		
-		@Before
-		public void setUp() {
-			model = new SignInPageModel();
-			controller = new SignInPageController();
+	@Before
+	public void setUp() {
+		model = new SignInPageModel();
+		controller = new SignInPageController();
 			
-			model.setGivenUsername("Clever Username");
-			model.setGivenPassword("Strong Password");
+		model.setGivenUsername("Clever Username");
+		model.setGivenPassword("Strong Password");
 			
-			controller.setModel(model);
-		}
+		controller.setModel(model);
+	}
 		
-		@Test
-		public void testMultiply() {
-			try {
-				controller.deleteAccount();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			assertEquals(model.checkForUser("Clever Username"), false);
+	@Test
+	public void testMultiply() {
+		try {
+			controller.deleteAccount();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		assertEquals(model.checkForUser("Clever Username"), false);
 	}
 }
