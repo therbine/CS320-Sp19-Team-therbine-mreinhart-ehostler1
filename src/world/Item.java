162e6c601eb422d093701658ce8af8fbd42c8fa7
Thomas Herbine <therbine@ycp.edu>
2019-03-28 17:41:37 -0400

    Added some of the classes for the game

---
+++ src/world/Item.java
@@ -0,0 +1,12 @@
+package world;
+
+public class Item {
+	private ItemType type;
+	private int dmg;
+	private int health;
+	private int armor;
+	
+	public Item() {
+		
+	}
+}
---
+++ src/world/Room.java
@@ -0,0 +1,12 @@
+package world;
+
+import java.util.ArrayList;
+
+public class Room {
+	private Terrain terrain;
+	private ArrayList<Item> roomInv;
+	
+	public Room() {
+		
+	}
+}
