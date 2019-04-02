0762ae75f8b42ea66fbe2f806c0b8dea30118654
Ethan Hostler <ehostler1@ycp.edu>
2019-04-02 17:57:06 -0400

    Restructured controllers models and servlets so that data for multiple users can be stored

---
+++ src/controller/UserDataController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import model.UserDataModel;
+
+public class UserDataController {
+	private UserDataModel model;
+	
+	public void setModel(UserDataModel model) {
+		this.model = model;
+	}
+	
+	public void updateHistory() {
+		model.addHistory(model.getPlayerInput());
+	}
+	
+	public void updateGameDisplay() {
+		String newDisplay = "";
+		for(int i = 0 ; i < model.getHistorySize() ; i++) {
+			newDisplay += "<p>" + model.getHistory(i) + "</p>";
+		}
+		model.setGameDisplay(newDisplay);
+	}
+}
