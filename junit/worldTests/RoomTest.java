49591ca3ef8b1036a6c49a81b7719fe83fc89fbd
Thomas Herbine <therbine@ycp.edu>
2019-04-02 16:25:41 -0400

    Added test cases for room and item

---
+++ junit/worldTests/RoomTest.java
@@ -0,0 +1,63 @@
+package worldTests;
+
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+import world.*;
+
+public class RoomTest {
+	private Room room;
+	
+	@Before
+	public void setUp() {
+		Terrain terrain = Terrain.beach;
+		ArrayList<Item> roomInv = new ArrayList<Item>();
+		
+		roomInv.add(new Item(ItemType.armor, 0, 0, 5));
+		roomInv.add(new Item(ItemType.potion, 0, 6, 0));
+		roomInv.add(new Item(ItemType.weapon, 3, 0, 0));
+		
+		room = new Room(terrain, roomInv);
+	}
+	
+	@Test
+	public void testGetInv() {
+		Item item1 = room.getInv().get(0);
+		Item item2 = room.getInv().get(1);
+		Item item3 = room.getInv().get(2);
+		
+		assertEquals(item1.getVal(), 5);
+		assertEquals(item2.getVal(), 6);
+		assertEquals(item3.getVal(), 3);
+	}
+	
+	@Test
+	public void testGetTerrain() {
+		Terrain terrain = room.getTerrain();
+		assertEquals(terrain, Terrain.beach);
+	}
+	
+	@Test
+	public void testTakeItem() {
+		int index = 1;
+		Item toTake = room.getInv().get(index);
+		int sizeBefore = room.getInv().size();
+		
+		Item taken = room.takeItem(index);
+		int sizeAfter = room.getInv().size();
+		
+		assertEquals(toTake, taken);
+		assertEquals(sizeBefore - 1, sizeAfter);
+	}
+	
+	@Test
+	public void testAddItem() {
+		Item toAdd = new Item(ItemType.potion, 0 , 1, 0);
+		room.addItem(toAdd);
+		
+		assertEquals(toAdd, room.getInv().get(room.getInv().size() - 1));
+	}
+}
