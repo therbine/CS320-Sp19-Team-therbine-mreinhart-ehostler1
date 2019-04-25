cfab6f82333e75851c19097e7f8dbca0539cb1ff
Thomas Herbine <therbine@ycp.edu>
2019-04-25 17:16:44 -0400

    Added methods to IDatabase

--- src/database/persist/IDatabase.java
+++ src/database/persist/IDatabase.java
@@ -1,10 +1,11 @@
 package database.persist;
 
-import model.UserDataModel;
 import java.util.List;
 
 public interface IDatabase {
 	public List<String> PasswordByUsernameQuery(String username);
 	public List<byte[]> UserDataByUsernameQuery(String username);
 	public Integer insertNewUser(String username, String password);
+	public List<Account> removeAccountByUsername(String username);
+	public Integer updateUserData(String username, byte[] bytes);
 }
